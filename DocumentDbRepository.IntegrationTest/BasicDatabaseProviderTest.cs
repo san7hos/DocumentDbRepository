2623507e0e29b2bb07be2ef4c550728289865703
Santhos <michal.stasa@designeo.cz>
2015-12-11 18:43:10 +0100

    Structure for integration tests added

---
+++ DocumentDbRepository.IntegrationTest/BasicDatabaseProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DocumentDbRepository.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DocumentDbRepository.IntegrationTest/DocumentClientFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Santhos.DocumentDb.Repository.IntegrationTest
+{
+    using System;
+
+    using Microsoft.Azure.Documents.Client;
+
+    internal class DocumentClientFactory
+    {
+        public DocumentClient Create()
+        {
+            throw new NotImplementedException();
+            //return new DocumentClient();
+        }
+    }
+}
